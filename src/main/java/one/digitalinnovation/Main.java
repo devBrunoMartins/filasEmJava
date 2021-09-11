f3a52b78cd7fa7ed3988ce339517bddb3d821504
Bruno martins <bruno.eletr@gmail.com>
2021-09-11 11:54:43 -0300

    Implementando o método Main

--- src/main/java/one/digitalinnovation/Main.java
+++ src/main/java/one/digitalinnovation/Main.java
@@ -1,4 +1,29 @@
 package one.digitalinnovation;
 
 public class Main {
+
+    public static void main(String[] args) {
+        Fila minhaFila = new Fila();
+
+        minhaFila.enqueue(new No("Primeiro"));
+        minhaFila.enqueue(new No("Segundo"));
+        minhaFila.enqueue(new No("Terceiro"));
+        minhaFila.enqueue(new No("Quarto"));
+
+        System.out.println(minhaFila);
+
+        System.out.println(minhaFila.dequeue());
+        System.out.println(minhaFila);
+
+        minhaFila.enqueue(new No("Último"));
+
+        System.out.println(minhaFila);
+
+        System.out.println(minhaFila.first());
+
+        System.out.println(minhaFila);
+
+
+    }
+
 }
